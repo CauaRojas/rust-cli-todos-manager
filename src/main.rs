b08846812b803d673c23edba87a0caa5f3a163c4
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-07-12 22:29:53 -0300

    added basic saving and loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use serde::{Deserialize, Serialize};
+use std::env::args;
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use std::vec;
+
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    desc: String,
+}
+fn save_state(todos: Vec<Todo>) -> std::io::Result<()> {
+    let file = File::create("todos.json")?;
+    let writer = BufWriter::new(file);
+    serde_json::to_writer(writer, &todos)?;
+    Ok(())
+}
+fn get_saved_state() -> Vec<Todo> {
+    let file = match File::open("todos.json") {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Local todo file not found!\nCreating a new one...");
+            return vec![];
+        }
+    };
+    let reader = BufReader::new(file);
+    match serde_json::from_reader(reader) {
+        Ok(todos) => todos,
+        Err(_) => {
+            println!("Local todo file not found!\nCreating a new one...");
+            vec![]
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    /*
+     * ADD
+     * REMOVE
+     * LIST
+     * HELP
+     */
+    save_state(get_saved_state()).expect("Could not save");
+    for arg in args() {
+        println!("{}", arg)
+    }
 }
